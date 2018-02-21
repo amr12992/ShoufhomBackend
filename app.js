9cc836f94cc2fcb22fb80f8991b2da6e22e36e57
iam <iam@hamooding.com>
2018-02-21 05:15:15 -0500

    Added SQL inject prevention, signup, and some comments.

--- app.js
+++ app.js
@@ -14,24 +14,26 @@ const sqlcon = mysql.createConnection({
   database : 'shoufhomDB'
 });
 
-
-app.get ('/test', function(req, res){
+//Get test case
+app.get ('/test', function(req, res){ 
   console.log('test requested');
   res.end('GET working');
 });
 
+//Login for all users types
 app.post ('/login', function(req, res){
-  //console.log(req.body.name);
   console.log('login request');
-  var username = req.body.username;
+  var civilID = req.body.civilID;
   var password = req.body.password;
-  sqlcon.query("SELECT * FROM USERS WHERE civilID = " + JSON.stringify(username) + ";", function(err, data, fields){
+  sqlcon.query("SELECT * FROM USERS WHERE civilID = ?;",[civilID], function(err, data, fields){
     if (err) throw err;
     
+    //Checks if username exists
     if (data[0] == null){
       res.json("incorrect_user");
-      console.log("Incorrect Username");
+      console.log("Incorrect civilID");
     }
+    //Check if password matches
     else if (JSON.stringify(data[0].password) === JSON.stringify(password)){
       
       res.json({
@@ -45,17 +47,47 @@ app.post ('/login', function(req, res){
       res.json("incorrect_password");
     }
   })
-
-  /*
-  var testres = {
-    "action":"success",
-    "usertype":"type"
-  };
-
-  res.json("received");
-  */
 });
 
+//Create new teacher or guardian
+app.post ('/signup', function(req, res){
+  console.log('login request');
+  var civilID = req.body.civilID;
+  var fName = req.body.fName;
+  var mName = req.body.mName;
+  var lName = req.body.lName;
+  var password = req.body.password;
+  var email = req.body.email;
+  var phone = req.body.phone;
+  var mobile = req.body.mobile;
+  var userRole = req.body.userRole;
+  
+  //Retrieves civilID from database to check if it already exists
+  sqlcon.query("SELECT * FROM USERS WHERE civilID = ?;", [civilID], function(err, data, fields){
+    if (err) throw err;
+    
+    //Tests if the username exists
+    if (data[0]) {
+      res.json("ID already exists");
+    }
+    
+    else{
+      //Retrieves highest userID and increments it by 1
+      sqlcon.query("SELECT MAX(userID) AS max_userID FROM USERS;", function(err, data, fields){
+        if (err) throw err;
+        var userID = Number(data[0].max_userID)+1;
+        
+        //Creates a new user
+        sqlcon.query("INSERT INTO USERS VALUES(?,?,?,?,?,?,?,?,?,?);", [civilID, userID, fName, mName, lName, password, email, phone, mobile, userRole], function(err, data, fields){
+          if (err) throw err;      
+          res.json("Signup Success");
+        })
+      })
+    }
+  })
+});
+
+//Initializes server
 app.listen(3000, function(){
   console.log('Listening on port 3000');
 })
