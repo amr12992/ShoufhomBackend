451fea8914542dd1b04b23fe7d54d58caa778577
iam <iam@hamooding.com>
2018-05-01 09:00:08 -0400

    Final submission commit, too many changes to list.

--- SQL/0shoufhom_build.sql
+++ SQL/0shoufhom_build.sql
@@ -38,17 +38,18 @@ CREATE TABLE STUDENTS(
   fName varchar(25) NOT NULL,
   mName varchar(25) NOT NULL,
   lName varchar(25) NOT NULL,
-  guardianID varchar(8) NOT NULL,
+  guardianCivilID varchar(12) NOT NULL,
+  gradeFlag tinyint NOT NULL DEFAULT 0,
+  absenceFlag tinyint NOT NULL DEFAULT 0,
+  activeFlag tinyint NOT NULL DEFAULT 1,
 
-  PRIMARY KEY (civilID),
-  FOREIGN KEY (guardianID) REFERENCES USERS(userID)
+  PRIMARY KEY (civilID)
 );
 
 CREATE TABLE SUBJECTS(
   subjectName varchar(50),
   classID varchar(8),
   subjectID varchar(8) UNIQUE NOT NULL,
-  subjectType varchar(50) NOT NULL,
   teacherID varchar(8) NOT NULL,
   examTime1 DATETIME,
   examTime2 DATETIME,
@@ -65,22 +66,12 @@ CREATE TABLE SUBJECTSTUDENT(
   subjectID varchar(8),
   studentID varchar(8),
   subjectStudentID varchar(8) UNIQUE NOT NULL,
-  grade1 varchar(3),
-  grade2 varchar(3),
-  grade3 varchar(3),
-  grade4 varchar(3),
-  grade5 varchar(3),
-  grade6 varchar(3),
-  grade7 varchar(3),
-  grade8 varchar(3),
-  grade9 varchar(3),
-  grade10 varchar(3),
-  grade11 varchar(3),
-  grade12 varchar(3),
-  grade13 varchar(3),
-  grade14 varchar(3),
-  grade15 varchar(3),
-
+  quarter1 varchar(3),
+  quarter2 varchar(3),
+  quarter3 varchar(3),
+  quarter4 varchar(3),
+  finalGrade varchar(3),
+  
   PRIMARY KEY (subjectID, studentID),
   FOREIGN KEY (subjectID) REFERENCES SUBJECTS(subjectID),
   FOREIGN KEY (studentID) REFERENCES STUDENTS(studentID)
@@ -90,7 +81,7 @@ CREATE TABLE ABSENCES(
   subjectStudentID varchar(8),
   absenceDate DATE,
 
-  PRIMARY KEY (subjectStudentID),
+  PRIMARY KEY (subjectStudentID, absenceDate),
   FOREIGN KEY (subjectStudentID) REFERENCES SUBJECTSTUDENT(subjectStudentID)
 );
 
